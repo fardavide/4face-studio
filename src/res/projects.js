7e2f75616b461f5d1ec868dedf6364d64e95a6cd
Davide Giuseppe Farella <37957092+4face-studi0@users.noreply.github.com>
2019-05-01 18:06:14 +0200

    Update projects.js

--- src/res/projects.js
+++ src/res/projects.js
@@ -21,7 +21,8 @@ const projects = {
     title: "Fresh Tv",
     description: {
       en: `is an app for watch **IPTV**, born with the goal of create a new app, simple for every one and with a _"FRESH"_ design!`,
-      it: `è un app per guardare l'**IPTV**, nata con l'obiettivo di creare una nuova app, semplice per chiunque e con uno stile _"FRESH"_!`
+      it: `è un app per guardare l'**IPTV**, nata con l'obiettivo di creare una nuova app, semplice per chiunque e con uno stile _"FRESH"_!`,
+      cn: `是一个可以观看**IPTV**的手机软件, 以创建新应用程序为目标而生,_"FRESH"_ 的设计更简单更实用`,
     },
     icon: freshTvIcon,
     link: "https://github.com/4face-studi0/FreshTv-android",
@@ -35,7 +36,10 @@ const projects = {
       en:
         "is a **lightweight image loader** for Android with a _cache system_ and built-in **Kotlin DSL**",
       it:
-        "è un **leggerissimo image loader** con un _sistema di cache_ e con **Kotlin DSL** integrato"
+        "è un **leggerissimo image loader** con un _sistema di cache_ e con **Kotlin DSL** integrato",
+      cn: 
+        "是一个**轻巧型图像加载程序** 适用于带有高速缓存系统以及**Kotlin DSL**的安卓系统",
+      
     },
     link: "https://github.com/4face-studi0/Theia",
     type: projectType.lib
@@ -48,7 +52,9 @@ const projects = {
       en:
         "permits to handle an **unidirectional data-flow** to la UI, wrapping a _LiveData_.",
       it:
-        "permette di gestire un **flusso dati unidirezionale** verso la UI, wrappando un _LiveData_."
+        "permette di gestire un **flusso dati unidirezionale** verso la UI, wrappando un _LiveData_.",
+      cn: 
+        "允许处理**单向数据流**进入用户界面，包装_LiveData_.",
     },
     link: "https://github.com/4face-studi0/ViewStateStore",
     type: projectType.lib
@@ -61,7 +67,9 @@ const projects = {
       en:
         "lets you implement a customized _BottomAppBar_ and a **bottom drawer** with just few simple steps.",
       it:
-        "permette di implmentare una _BottomAppBar_ custom con un **bottom drawer** in pochi semplici step."
+        "permette di implmentare una _BottomAppBar_ custom con un **bottom drawer** in pochi semplici step.",
+      cn:
+        "让你实现自定义的_BottomAppBar_ 以及只需简单的几个步骤就可以完成**底部菜单** 
     },
     link: "https://github.com/4face-studi0/MaterialBottomBar",
     type: projectType.lib
@@ -72,7 +80,8 @@ const projects = {
     title: "Fluent Notifications",
     description: {
       en: "is an idiomatic **Kotlin DSL** for create _Android's Notification_",
-      it: "è un **Kotlin DSL** idiomatico per creare _Notifiche su Android_"
+      it: "è un **Kotlin DSL** idiomatico per creare _Notifiche su Android_",
+      cn: "是为了制作安卓提示的一个**Kotlin DSL**语言
     },
     link: "https://github.com/4face-studi0/FluentNotifications",
     type: projectType.lib
