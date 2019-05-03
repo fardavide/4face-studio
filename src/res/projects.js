12f3335d572764e98f6f35737161e474cfe49c2a
Davide Giuseppe Farella <37957092+4face-studi0@users.noreply.github.com>
2019-05-03 17:12:12 +0200

    Update projects.js

--- src/res/projects.js
+++ src/res/projects.js
@@ -23,6 +23,7 @@ const projects = {
       en: `is an app for watch **IPTV**, born with the goal of create a new app, simple for every one and with a _"FRESH"_ design!`,
       it: `è un app per guardare l'**IPTV**, nata con l'obiettivo di creare una nuova app, semplice per chiunque e con uno stile _"FRESH"_!`,
       cn: `可以观看**IPTV**的手机软件, 以创建新应用程序为目标而生,_"FRESH"_ 的设计更简单更实用`,
+      jp: `IPTVを見ることができるアプリケーションです。新しく、フレッシュなデザインで、もっと簡単で実用的なアプリケーションです。
     },
     icon: freshTvIcon,
     link: "https://github.com/4face-studi0/FreshTv-android",
@@ -39,6 +40,8 @@ const projects = {
         "è un **leggerissimo image loader** con un _sistema di cache_ e con **Kotlin DSL** integrato",
       cn: 
         "**轻巧型图像加载程序** 适用于带有高速缓存系统以及**Kotlin DSL**的安卓系统",
+　　　 jp:
+        "キャッシュシステムと**Kotlin DSL**で作られたAndroid用の**軽量イメージローダ**",
       
     },
     link: "https://github.com/4face-studi0/Theia",
@@ -55,6 +58,9 @@ const projects = {
         "permette di gestire un **flusso dati unidirezionale** verso la UI, wrappando un _LiveData_.",
       cn: 
         "允许处理**单向数据流**进入用户界面，包装_LiveData_.",
+      jp: 
+        "LiveDataを包装し、la UIへの単方向データフローを処理することが許可される。
+       
     },
     link: "https://github.com/4face-studi0/ViewStateStore",
     type: projectType.lib
@@ -70,6 +76,8 @@ const projects = {
         "permette di implmentare una _BottomAppBar_ custom con un **bottom drawer** in pochi semplici step.",
       cn:
         "让你实现自定义的_BottomAppBar_ 以及只需简单的几个步骤就可以完成**底部菜单**"
+      jp:
+        "カスタマイズされた_BottomAppBar_を実現して、少々簡単なステップで、**bottom drawer**を完成できます。
     },
     link: "https://github.com/4face-studi0/MaterialBottomBar",
     type: projectType.lib
@@ -82,6 +90,7 @@ const projects = {
       en: "is an idiomatic **Kotlin DSL** for create _Android's Notification_",
       it: "è un **Kotlin DSL** idiomatico per creare _Notifiche su Android_",
       cn: "为了制作安卓提示的一个**Kotlin DSL**语言"
+　　　 jp: "アンドロイドのお知らせを作るための一つの**Kotlin DSL**です。
     },
     link: "https://github.com/4face-studi0/FluentNotifications",
     type: projectType.lib
